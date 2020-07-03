6c6f9d793dec4cd6bab57a95c393b5eadb42aaf3
Max Vyaznikov <maxvyaznikov@gmail.com>
2020-07-03 14:45:58 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+Examples of Selectel Cloud Functions usage
+===
+
+* [Email sending](https://github.com/selectel/cloud-functions-examples/blob/master/mail_sender.py)
+* [Telegram bot](https://github.com/selectel/cloud-telegram-bot)
---
+++ mail_sender.py
@@ -0,0 +1,30 @@
+import os
+import json
+import smtplib
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.yandex.ru')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+EMAIL_TO = os.environ.get('EMAIL_TO')
+
+
+def main(**kwargs):
+    text = json.dumps(kwargs, indent=2)
+
+    server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
+    server.ehlo()
+    server.starttls()
+    server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+    message = "\r\n".join([
+        f"From: {EMAIL_HOST_USER}",
+        f"To: {EMAIL_TO}",
+        "Subject: Serverless Form",
+        "",
+        str(text)
+    ])
+    server.set_debuglevel(1)
+    server.sendmail(EMAIL_HOST_USER, EMAIL_TO, message)
+    server.quit()
+    return f"Email was sent"
