be1cdd7169a0c543daba042f55bd3278eb2a3e98
Max Vyaznikov <vyaznikov@selectel.ru>
2020-10-12 15:37:50 +0300

    pdf_merge keep connection between multiple requests

--- pdf_merge/pdf_merge/main.py
+++ pdf_merge/pdf_merge/main.py
@@ -3,17 +3,17 @@ import swiftclient
 from io import BytesIO
 from PyPDF2 import PdfFileMerger
 
+# Connect to Selectel Cloud Storage.
+conn = swiftclient.client.Connection(
+    authurl='https://api.selcdn.ru/auth/v1.0',
+    user=os.environ.get("SELECTEL_STORAGE_CLIENT_NAME"),
+    key=os.environ.get("SELECTEL_STORAGE_CLIENT_PASSWORD"),
+    auth_version=1,
+)
+
 
 def main(container_name, obj_a, obj_b):
-    # Connect to Selectel Cloud Storage.
-    conn = swiftclient.client.Connection(
-        authurl='https://api.selcdn.ru/auth/v1.0',
-        user=os.environ.get("SELECTEL_STORAGE_CLIENT_NAME"),
-        key=os.environ.get("SELECTEL_STORAGE_CLIENT_PASSWORD"),
-        auth_version=1,
-    )
-
-    # Fetch image.
+    # Fetch objects.
     _, obj_a_body = conn.get_object(container_name, obj_a)
     _, obj_b_body = conn.get_object(container_name, obj_b)
     obj_a_bytes = BytesIO(obj_a_body)
@@ -29,7 +29,7 @@ def main(container_name, obj_a, obj_b):
     file_name_b, _ = os.path.splitext(obj_b)
     obj_new_name = f"{file_name_a}_{file_name_b}_merged.pdf"
 
-    # Upload new image.
+    # Upload new object.
     result_bytes = BytesIO()
     merger.write(result_bytes)
     result_bytes.seek(0)
