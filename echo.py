d2ef8b83c0f13a790c4caa4d08213dd3128f53da
Max Vyaznikov <vyaznikov@selectel.ru>
2020-10-29 10:17:49 +0300

    Added activation of handler-function for local code run in Echo example

--- echo.py
+++ echo.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import datetime
 
 container_started_at = datetime.now()
@@ -9,3 +10,11 @@ def main(**kwargs):
         "now": datetime.now().isoformat(),
         "container_started_at": container_started_at.isoformat(),
     }
+
+
+# Call handler-function if code runs locally.
+# $ python echo.py param="param pam pam" a=1
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    kwargs = dict([arg.split('=') for arg in args]) if args else {}
+    print(main(**kwargs))
