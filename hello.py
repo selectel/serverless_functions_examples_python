39909640d54e914f3d582719ef17b591e6e00648
Max Vyaznikov <vyaznikov@selectel.ru>
2020-11-03 15:06:28 +0300

    Added hello.py (very simple example)

---
+++ hello.py
@@ -0,0 +1,10 @@
+
+def main(**kwargs):
+    """
+    Это вызываемая функция.
+    Она будет выполняться для каждого отдельного запроса.
+
+    This is function to execute.
+    It will be invoked for each request.
+    """
+    return f"Hello, {kwargs}!"
