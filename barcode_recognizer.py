52d8649c3c4a613edc7c393fa56ec15611f5bdc0
Max Vyaznikov <vyaznikov@selectel.ru>
2020-11-03 15:37:54 +0300

    Fixed comments into barcode_recognizer.py and erased debug code

--- barcode_recognizer.py
+++ barcode_recognizer.py
@@ -3,18 +3,24 @@ import urllib.request
 from pyzbar.pyzbar import decode
 from PIL import Image
 
+"""
+Функция создана только для выполнения в среде "Python с библиотеками для
+обработки изображений", так как зависит от предустановленной в ней
+библиотеки libzbar (https://github.com/herbyme/zbar).
 
-# For runtime "Python 3.7 with libraries for image processing" only.
-# In standard python runtime it will not work because on dependency on libzbar.
+Function is for runtime "Python with libraries for image processing" only
+because of dependency on libzbar (https://github.com/herbyme/zbar) preinstalled
+only there.
+"""
 
 
 def main(image_url):
+    """
+    Функция ожидает на вход URL с изображением.
+
+    Function expects an URL with image.
+    """
     with BytesIO() as img_bytes:
         img_bytes.write(urllib.request.urlopen(image_url).read())
         decoded = decode(Image.open(img_bytes))
         return {"codes": [d.data.decode('utf-8') for d in decoded]}
-
-
-if __name__ == "__main__":
-    # Just for local testing.
-    print(f"{main(image_url='https://418511.selcdn.ru/images/kabachki.jpg')}")
