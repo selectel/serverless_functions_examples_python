6325f69023c457fd82b6a6661be684107ad0653f
Max Vyaznikov <maxvyaznikov@users.noreply.github.com>
2020-11-05 12:43:30 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,11 @@ def main(**kwargs):
     Это вызываемая функция.
     Она будет выполняться для каждого отдельного запроса.
 
+    @param kwargs - входные параметры
+
     This is function to execute.
     It will be invoked for each request.
+
+    @param kwargs - input parameters
     """
     return f"Hello, {kwargs}!"
